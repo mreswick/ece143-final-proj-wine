34f117d809d7c64d55c552d40826e43ad654f4bb
Mive667 <114213661+Mive667@users.noreply.github.com>
2023-03-17 17:33:34 -0700

    Add files via upload

--- wine_stat/vis.py
+++ wine_stat/vis.py
@@ -3,6 +3,12 @@ from wine_stat import freq
 import seaborn as sns
 import matplotlib.pyplot as plt
 import colorcet as cc
+from database import db_constants
+import sqlite3 as sl
+import plotly.express as px
+from pyecharts.charts import Map
+from pyecharts import options as opts
+
 
 """Module for visualizing data.
 """ 
@@ -262,3 +268,113 @@ def plot_pie_charts_all_top_n_freq_tables(
   for (_, dict_top_n_freq_tables) in dict_freq_tables.items():
     #plot the pie charts for those top_n tables with n=n_i, of ie, for those n_i
     plot_pie_charts_for_freq_tables(cur, con, dict_top_n_freq_tables)
+
+# ----------------------------------------------- Overview ----------------------
+def readin_db_to_df(path='database/'):
+  # read in database
+  WINE_INIT_DB_NAME = db_constants.WINE_INIT_DB_NAME
+  WINE_INIT_PATH_TO_DB = path + WINE_INIT_DB_NAME
+  con = sl.connect(WINE_INIT_PATH_TO_DB)
+  sql_cmd = "SELECT * FROM wine_init"
+  df = pd.read_sql_query(sql_cmd, con, index_col = 'index')
+  con.close()
+  return df
+
+def sunBurstChart_winecount(df=readin_db_to_df(), color='Bugn'):
+  # wine_count under country -> province -> region
+  # pick 3 columns and do freq count
+  df_CnPR = df[['country', 'province','region_1']].value_counts()
+  # organize the dataframe
+  df_CnPR = pd.DataFrame(df_CnPR).reset_index().rename(columns = {0 : 'wine_count'})
+  # drawing plot:
+  fig=px.sunburst(df_CnPR,path=['country','province','region_1'],values='wine_count',color='wine_count',color_continuous_scale=color)
+  fig.update_traces(textinfo='label + percent parent + value')
+
+def price_point_all(df=readin_db_to_df()):
+  title = "Relationship between price and point"
+  plt.figure(figsize = [14,6])
+  ax = sns.regplot(x = df["price"], y="points", data=df, logx=True, truncate=True)
+  plt.title(title,fontsize='x-large')
+  plt.tight_layout()
+  # plt.savefig(f'./{title}.jpg',dpi=300)
+
+def price_point_province(df=readin_db_to_df(), n=3):
+  topProvince = df['province'].value_counts(normalize = True).head(n).index.tolist()
+  df_plimit = df[df['price']<=100]
+  count = 1
+  for p in topProvince:
+    title = f'Hex grid of price and point in {p}'
+    plt.figure(figsize = [14,6])
+    ax = sns.jointplot(x='price', y='points', data=df_plimit.loc[df_plimit['province'].isin([p])], kind='hex',gridsize=20)
+    plt.suptitle(title)
+#     plt.savefig(f'./0{count}-{title}.jpg',dpi=300)
+    count += 1
+
+def price_point_variety(df=readin_db_to_df(), n=3):
+  df_plimit = df[df['price']<=100]
+  topVariety = df['variety'].value_counts(normalize = True).head(n).index.tolist()
+  count = 1
+  for v in topVariety:
+      title = f'Hex grid of price and point in {v}'
+      plt.figure(figsize = [14,6])
+      ax = sns.jointplot(x='price', y='points', data=df_plimit.loc[df_plimit['variety'].isin([v])], kind='hex',gridsize=20)
+      plt.suptitle(title)
+  #     plt.savefig(f'./0{count}-{title}.jpg',dpi=300)
+      count += 1
+
+# ----------------------------------------Overview Map --------------------------
+def count_Country(df=readin_db_to_df()):
+  #get the data that can be read in 'echarts' (echarts.apache.org)
+  df_map = df[df['price'] <= 1000]
+  df_map.replace("US","United States",inplace=True)
+  df_map.replace("England","United Kingdom",inplace=True)
+  df_map.replace("Czech Republic","Czech Rep.",inplace=True)
+  df_map.replace("Bosnia and Herzegovina","Bosnia and Herz.",inplace=True)
+
+  country=list(df_map['country'].value_counts().index)
+  counts=list(df_map['country'].value_counts().values)
+
+  a="'name'"
+  b="'value'"
+  data=[country,counts]
+  CountsPerCountry=[{eval(a):data[0][i],eval(b):data[1][i]} for i in range(len(country))]
+  link_str = '''
+  the chart can be displayed at the link below:
+  https://echarts.apache.org/examples/en/editor.html?c=map-usa&code=LYTwwgFghgTgLgOgM4QPYHcAyqoBMCWAdgOYAUAlANwBQAJAsQKZykBKA8uwCoD6ACgEEuACQAEAalEByAPS4ocKDKhIkzGU1Qz0qGABtcCAFZJUhKQBpRAMwCuhAMZx8Z0aVtIoAKVOFyogG9qUVFQSFhECHxcRmw8IjIqYNFGB2h4JAQYRmJ8JDhGGABZKAAHUikAVQBlAUtRD29fKyCQkIE9FQBrKAAuQOS20T1Gazh-gFoARgBmKYtBtrhUUv6AJgBWBaGQ9Gi4CH6pjcWAX2224Sh0KHx8ftadkbHJqamABguh5dXRNYAOL5tPa4A79E5Dc6LKR8WyFZaiVj4ByoKQPRYhZ7jUQTADsADYgSEfutCRjRCCwX8zslTkkQitnK4ALwDb74OAjdE7UQFAAe2KkAHUiIxEYwAG74RjoURgVD2OBIURC3QGEGMSzkrH9KQwfDECBwKRnIHLVB6Zy_R7ffXEJgwXUcxjALU8lAYAAijE6IH6n3JcBgUEISA5LkIntswaZhH9CDWpsWUqQtigehK1u1o0FdqNbp2wCIRyJoSgfPBABY1vjK6WiKwQ0xuTzRCi9Lp-gBtclDKQAYjmM3xAE4NgXW9J-5WNriNgAjSsT1sD3GVvC4KbLnkDqDz3Ajxgj7c7AeMd7WGbWf4nvv96wP6zz6y3toD6yMc8j96vkLv-SMPiW6lne1iVviuCVjMv5TrguIzO8ay4jBu4bO8iH4iak4ALrklCPL8tiXZSMIBoQPUUjYOgUjYaWDjpg4tidPOXK8jAcJJt8qAWvOqAVmyOweug_RBnCpYyDIujSoQgoSvCyLpieOrSFiJ4ktIPwnh-CjRowLY8vIigAGrSsJgSiNkeDsIQeh-jY6ZqKI-Gttk-S6HpgTOe6UByQISAAJLAFAzaeXhnFtGo-qMEg3bkjaOyEFAwAecKorilKMpygqMlIK-cAgKUKVBaUr4wDgwAiexjAgWWvxVLUr4uqU0BhjFAmTsxPr6ZOohCZVHE9U5vZOTVhl9KIXYBFIiXJWi0iVIQzq4KI1SKAUuVWFIErpnCc0bNWtbnIE01JZquoAGLBo4mqbdtei7UcuIEpWR1TTNZ3SP5ii2RRd0PaIUyjlML0tCds26tUpS3OYt07R9-KzjMr1gx9MK6HAtjEIpsP3R9lb_HOyPvXNUiQPgIy_XDc2VpWgNE6dJMCDATAyUQUCU7jc0zHO-L0-D0gCB4Qb4OzOP_UhI7_HzqOC_kwZ6CLHPi2sI4g8dxO6gA4oUQWECASsfWsUxrO80skwAcplABajDpiGuAG3Nsy4lLoMa9I1QKgcogCNY-r0Y7Rwq0jbsM7q_lIMGPqB6I-MjmbWvZKkN3SH9eNAQn0hgCGeCi6nVPrPtmckfYWMwPrYsfcDGzFwAQkxZeK5XTu08XrCoLrTf55zQem6H_NVOxxBphX3f_R88f96jXDRl0jCj1tBeiN-xfa7ouR54vPeiP8atvWHHsdnJi2b2nc3_H36sH6TZUKF3W__biUzF0UjB8siqLN_0uKX_vA8LUtUQABpBIuAO4x1HC_C0YDtoxw2JPK-A9a7BgAF7kxjpWXExdMCMHnCGMwMcZg1yniTIouhUAOBRDHAExc-CFFsDHOmJDdSVC6MGUUjC94oxJtUQo8575nyDsXMAKDUgQHFKUBAjC1gvygA4RgYCT7SOEQVdiG0x5V2fswz6hACCnyXggv-qNqh7DgKI_Q9sY5YO0ZRWwfIXS8WjMQCBddUBIBPqIe2ohhCFBQVIr-fxi5M2SkogJMibHVCPlALoAil5aMQajMmiVGGnFwjyU4iw0lORoCEMI6REBqDgOwUosZSCMgjEkOklAgA
+  '''
+  print(link_str)
+
+def point_mean_description_length(df=readin_db_to_df()):
+  #get the correlation plot between Points and Mean Description Length and Mean Price group by tasters
+  df_points=df[df['price'] <= 1000]
+  df_points['description_length']=df_points['description'].str.len()
+
+  #get the correlation plot between Points and Mean Description Lengthacross the whole dataset 
+  sns.set_theme(style="darkgrid")
+  g=sns.lmplot(data=df_PointsMeanLength,x="description_length",y="points",scatter_kws={"s": 50, "alpha": .5})
+  g.set(xlim=(100,450))
+  title_="Correlation between Points and Mean Description Length"
+  plt.title(title_,fontsize='x-large')
+  #plt.savefig(f'./{title_}.jpg',dpi=300)
+
+  df_top5Taster = df_points[df_points["taster_name"].str.contains("Roger Voss|Michael Schachner|Kerin O’Keefe|Virginie Boone|Paul Gregutt")==True]
+  for taster in ["Roger Voss","Michael Schachner","Kerin O’Keefe","Virginie Boone","Paul Gregutt"]:
+      df_taster=df_top5Taster[df_top5Taster["taster_name"]==taster]
+      
+      df_PointsMeanLength = df_points.groupby("points").agg({'description_length':'mean'}).sort_values(by = 'description_length',ascending = False)
+      df_PointsMeanLength=df_PointsMeanLength.reset_index()
+      
+      df_PointsMeanPrice=df_taster.groupby("points").agg({'price':'mean'}).sort_values(by = 'price',ascending = False)
+      df_PointsMeanPrice=df_PointsMeanPrice.reset_index()
+      
+      g=sns.lmplot(data=df_PointsMeanLength,x="description_length",y="points",scatter_kws={"s": 50, "alpha": .5},logx=True)
+      g.set(xlim=(100,450))
+      title_="Mean Description Length Given Points - "+taster
+      plt.title(title_,fontsize='x-large')
+      #plt.savefig(f'./{title_}.jpg',dpi=300,bbox_inches='tight')
+      
+      g=sns.lmplot(data=df_PointsMeanPrice,x="price",y="points",scatter_kws={"s": 50, "alpha": .5,"color":"m"},line_kws={"linewidth": 3, "alpha": .5,"color":"m"},logx=True)
+      g.set(xlim=(0,600))
+      title_="Mean Price Given Points - "+taster
+      plt.title(title_,fontsize='x-large')
+      #plt.savefig(f'./{title_}.jpg',dpi=300,bbox_inches='tight')
